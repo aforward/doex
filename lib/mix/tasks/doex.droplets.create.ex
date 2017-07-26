8a9b5f72384cf96bf25061e0f6a4b7f8e5e99bf6
Andrew Forward <aforward@gmail.com>
2017-07-26 09:59:17 -0400

    Force newlines on documentation

--- lib/mix/tasks/doex.droplets.create.ex
+++ lib/mix/tasks/doex.droplets.create.ex
@@ -26,17 +26,25 @@ defmodule Mix.Tasks.Doex.Droplets.Create do
   For example
 
       mix doex.droplets.create mydroplet \
+
         --region tor1 \
+
         --tags myt \
+
         --image ubuntu-14-04-x64 \
+
         --size 512mb
 
   If you have a specific config file, `mix help doex.config` then add it as an environment variable
 
       DOEX_CONFIG=/tmp/my.doex mix doex.droplets.create mydroplet \
+
         --region tor1 \
+
         --tags myt \
+
         --image ubuntu-14-04-x64 \
+
         --size 512mb
 
   """
