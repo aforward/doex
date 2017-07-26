cde4316fff7751c464fcff24cb1e7bd4f1ea8cd5
Andrew Forward <aforward@gmail.com>
2017-07-25 21:54:15 -0400

    Incorporate version_tasks

--- mix.exs
+++ mix.exs
@@ -11,6 +11,7 @@ defmodule Doex.Mixfile do
     {:poison, "~> 3.1.0"},
     {:httpoison, "~> 0.11.1"},
     {:fn_expr, "~> 0.1.0"},
+    {:version_tasks, "~> 0.9.1"},
     {:ex_doc, ">= 0.0.0", only: :dev},
   ]
 
