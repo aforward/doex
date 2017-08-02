29320cf0b5b2d5ef493ab24678c189543397a08e
Andrew Forward <aforward@gmail.com>
2017-08-02 08:28:20 -0400

    Enhance Parser to merge in default values from the config

--- lib/doex/cli/parser.ex
+++ lib/doex/cli/parser.ex
@@ -1,17 +1,31 @@
 defmodule Doex.Cli.Parser do
   use FnExpr
 
-  def parse(raw_args, option_defn) do
+  def parse(raw_args, option_defn \\ %{}) do
     {opts, args, _} = OptionParser.parse(raw_args, switches: option_defn |> Map.to_list)
-    opts
+    config = Doex.Config.read
+
+    option_defn
+    |> Enum.map(fn {name, type} -> {name, defaulted(config, name, type)} end)
+    |> Enum.into(%{})
+    |> Map.merge(opts |> Enum.into(%{}))
     |> Enum.map(fn {k,v} ->
          case option_defn[k] do
-           :list -> {k, String.split(v, ",")}
+           :list -> {k, to_list(v)}
            _ -> {k, v}
          end
        end)
     |> Enum.into(%{})
     |> invoke({&1, args})
   end
+
+  defp defaulted(config, name, type), do: config[name] || defaulted(type)
+  defp defaulted(:boolean), do: false
+  defp defaulted(:string), do: nil
+  defp defaulted(:list), do: []
+
+  defp to_list(nil), do: []
+  defp to_list(l) when is_list(l), do: l
+  defp to_list(v) when is_binary(v), do: String.split(v, ",")
 
 end
---
+++ test/cli/parser_test.exs
@@ -0,0 +1,54 @@
+defmodule Doex.Cli.ParserTest do
+  use ExUnit.Case
+  alias Doex.Cli.Parser
+
+  setup do
+    on_exit fn ->
+      File.rm("/tmp/parser.doex")
+      System.delete_env("DOEX_CONFIG")
+      Doex.reload
+    end
+    :ok
+  end
+
+  @opt_defn %{one: :string, two: :boolean, four: :list}
+
+  test "parse empty" do
+    assert {%{}, []} == Parser.parse([] ,%{})
+    assert {%{}, ["dosomething"]} == Parser.parse(["dosomething"] ,%{})
+  end
+
+  test "parse string, boolean and list" do
+    args = ["dox", "--one" , "1", "--two", "three", "--four", "a,b,c"]
+    assert {%{four: ["a", "b", "c"], one: "1", two: true}, ["dox", "three"]}
+      == Parser.parse(args, @opt_defn)
+  end
+
+  test "defaulted values (no overrides)" do
+    args = ["dox", "three"]
+    assert {%{four: [], one: nil, two: false}, ["dox", "three"]}
+      == Parser.parse(args, @opt_defn)
+  end
+
+  test "no option definition" do
+    args = ["dox", "--a" , "1", "--b", "three", "--c", "a,b,c"]
+    assert {%{a: "1", b: "three", c: "a,b,c"}, ["dox"]}
+      == Parser.parse(args)
+  end
+
+  test "defaulted values (with overrides)" do
+    System.put_env("DOEX_CONFIG", "/tmp/parser.doex")
+    Doex.Config.put(:one, "uno")
+    Doex.Config.put(:two, true)
+    Doex.Config.put(:four, ["d", "e", "f"])
+
+    args = ["dox", "--one" , "1", "--two", "three", "--four", "a,b,c"]
+    assert {%{four: ["a", "b", "c"], one: "1", two: true}, ["dox", "three"]}
+      == Parser.parse(args, @opt_defn)
+
+    args = ["dox", "three"]
+    assert {%{four: ["d", "e", "f"], one: "uno", two: true}, ["dox", "three"]}
+      == Parser.parse(args, @opt_defn)
+  end
+
+end
