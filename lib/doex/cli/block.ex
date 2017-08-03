12aaa790fcf1c87518bbd1315bdf2e12689a51cb
Andrew Forward <aforward@gmail.com>
2017-08-03 09:46:57 -0400

    Support blocking on droplet statuses too

--- lib/doex/cli/block.ex
+++ lib/doex/cli/block.ex
@@ -5,13 +5,18 @@ defmodule Doex.Cli.Block do
   @moduledoc"""
   Block the command line until a condition is met,
 
-  Currently, we only support blocking on action statuses,
+  Currently, we support blocking on action statuses,
 
         doex block actions <id> <status>
+
+  And, droplet statuses
+
+        doex block droplets <id> <status>
 
   For example,
 
         doex block actions 12345 completed
+        doex block droplets 5672313 active
 
   The process is silent, so if you put in an invalid status,
   the script will run until manually stopped.
@@ -25,9 +30,22 @@ defmodule Doex.Cli.Block do
     |> block_until
   end
 
+  def block_until({_opts, ["droplets", id, status]} = input) do
+    Doex.Api.get("/droplets/#{id}")
+    |> invoke(fn {:ok, %{"droplet" => %{"status" => current_status}}} ->
+         block_until(current_status, status, input)
+       end)
+  end
+
   def block_until({_opts, ["actions", id, status]} = input) do
-    {:ok, %{"action" => %{"status" => current_status}}} = Doex.Api.get("/actions/#{id}")
-    if status != current_status do
+    Doex.Api.get("/actions/#{id}")
+    |> invoke(fn {:ok, %{"action" => %{"status" => current_status}}} ->
+         block_until(current_status, status, input)
+       end)
+  end
+
+  defp block_until(current_status, desired_status, input) do
+    if desired_status != current_status do
       :timer.sleep(10000)
       block_until(input)
     end
