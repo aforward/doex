e12b2b4a9c1324a3a2680dbae4718c87d5470194
Andrew Forward <aforward@gmail.com>
2017-08-09 08:39:46 -0400

    Update Shell.error, Shell.raise to consider --quiet arg

--- lib/doex/io/shell.ex
+++ lib/doex/io/shell.ex
@@ -15,23 +15,16 @@ defmodule Doex.Io.Shell do
     end
   end
 
-  def info(_msg, %{quiet: true}), do: nil
-  def info(msg, args) when is_list(args) do
-    if Enum.member?(args, "--quiet") do
-      nil
-    else
-      info(msg)
+  def info(raw_msg, args \\ %{}) do
+    case parse(raw_msg, args) do
+      {:quiet, msg} -> msg
+      {:ok, msg} -> if has_mix?() do
+                      Mix.shell.info(msg)
+                    else
+                      IO.puts(msg)
+                    end
     end
   end
-  def info(msg, _), do: info(msg)
-  def info(msg) when is_binary(msg) do
-    if has_mix?() do
-      Mix.shell.info(msg)
-    else
-      IO.puts(msg)
-    end
-  end
-  def info(msg), do: info("#{msg}")
 
   def inspect(data, %{quiet: true_or_false}), do: true_or_false |> _inspect(data)
   def inspect(data, args), do: Enum.member?(args, "--quiet") |> _inspect(data)
@@ -39,23 +32,43 @@ defmodule Doex.Io.Shell do
   defp _inspect(false, data), do: IO.inspect(data)
   defp _inspect(true, data), do: data
 
-  def error(msg) do
-    if has_mix?() do
-      Mix.shell.error(msg)
-    else
-      IO.puts(msg)
+  def error(raw_msg, args \\ %{}) do
+    case parse(raw_msg, args) do
+      {:quiet, msg} -> msg
+      {:ok, msg} -> if has_mix?() do
+                      Mix.shell.error(msg)
+                    else
+                      IO.puts(msg)
+                    end
     end
   end
 
-  def raise(msg) do
-    if has_mix?() do
-      Mix.raise(msg)
-    else
-      Kernel.raise(msg)
+  def raise(raw_msg, args \\ %{}) do
+    case parse(raw_msg, args) do
+      {:quiet, msg} -> msg
+      {:ok, msg} -> if has_mix?() do
+                      Mix.raise(msg)
+                    else
+                      Kernel.raise(msg)
+                    end
     end
   end
 
   def newline, do: info ""
 
   defp has_mix?, do: function_exported?(Mix, :shell, 1)
+
+  defp parse(msg, args), do: _parse(msg |> clean, args)
+  defp _parse(msg, %{quiet: true}), do: {:quiet, msg}
+  defp _parse(msg, args) when is_list(args) do
+    if Enum.member?(args, "--quiet") do
+      {:quiet, msg}
+    else
+      {:ok, msg}
+    end
+  end
+  defp _parse(msg, _), do: {:ok, msg}
+  defp clean(msg) when is_binary(msg), do: msg
+  defp clean(msg), do: "#{msg}"
+
 end
