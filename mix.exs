26476d9e445a34d67c503f4f18f831bf0c74be79
Andrew Forward <aforward@gmail.com>
2017-08-11 19:21:25 -0400

    Upgrade to fn_expr 0.2.*

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Doex.Mixfile do
     {:mix_test_watch, "~> 0.3", only: :dev, runtime: false},
     {:poison, "~> 3.1.0"},
     {:httpoison, "~> 0.11.1"},
-    {:fn_expr, "~> 0.1"},
+    {:fn_expr, "~> 0.2"},
     {:version_tasks, "~> 0.10"},
     {:ex_doc, ">= 0.0.0", only: :dev},
   ]
