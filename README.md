b13de494105d88df29ff9185f3b5a257828351b0
Andrew Forward <aforward@gmail.com>
2017-08-11 19:21:28 -0400

    v0.7.1

--- README.md
+++ README.md
@@ -95,7 +95,7 @@ to your `mix.exs` file.
 
 ```elixir
 @deps [
-  doex: "~> 0.7.0"
+  doex: "~> 0.7.1"
 ]
 ```
 
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Doex.Mixfile do
   @app :doex
   @git_url "https://github.com/capbash/doex"
   @home_url @git_url
-  @version "0.7.0"
+  @version "0.7.1"
 
   @deps [
     {:mix_test_watch, "~> 0.3", only: :dev, runtime: false},
