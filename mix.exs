e4101ce20badbc1b7f26586acf8060e8ee6d0dcf
Andrew Forward <aforward@gmail.com>
2020-01-10 08:48:43 -0500

    v0.10.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Doex.Mixfile do
   @app :doex
   @git_url "https://github.com/capbash/doex"
   @home_url @git_url
-  @version "0.10.1"
+  @version "0.10.2"
 
   @deps [
     {:mix_test_watch, "~> 0.7", only: :dev, runtime: false},
