7271161a897c4714ab8a356681efaf7384b714bc
Andrew Forward <aforward@gmail.com>
2020-02-29 12:21:03 -0500

    Move to elixir 1.10

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Doex.Mixfile do
     [
       app: @app,
       version: @version,
-      elixir: "~> 1.9",
+      elixir: "~> 1.10",
       name: @app,
       description: "A Digital Ocean API v2 client for Elixir (yes, another one)",
       package: @package,
